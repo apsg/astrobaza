ae48a277aab559a720c0bf3d74ddcce8817736a0
Szymon Gackowski <szymon.gackowski@gmail.com>
2020-10-27 20:27:17 +0100

    logos and images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
-    <cover img="{{ asset('images/cover.jpg') }}"></cover>
+    <cover img="{{ asset('images/cover.jpg') }}">
+        <div class="text-center h-100 d-flex flex-column justify-content-center">
+            <div>
+                <img src="{{ asset('images/logo_big_no_bg.png') }}">
+                <br />
+
+                <a href="{{ route('hours') }}" class="btn btn-secondary btn-lg mt-5">Zobacz godziny otwarcia</a>
+            </div>
+        </div>
+    </cover>
 
 @endsection
